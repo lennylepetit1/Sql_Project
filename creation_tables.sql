1f5ad339f6be58d9fd98f46890fd1bcbd10a564a
ThomasM59400 <thomas.meresse0956@outlook.fr>
2025-04-24 10:04:02 +0000

    table de base

--- creation_tables.sql
+++ creation_tables.sql
@@ -1,1 +1,17 @@
-CREATE 
+ CREATE TABLE base_de_table (
+  id INTEGER,
+  name TEXT,
+  sex TEXT,
+  age INTEGER,
+  height INTEGER,
+  weight INTEGER,
+  team TEXT,
+  noc TEXT,
+  games TEXT,
+  year INTEGER,
+  season TEXT,
+  city TEXT,
+  sport TEXT,
+  event TEXT,
+  medal TEXT
+);
